14d2f8171f867e1236bdd0f738616f607c33fc57
Anish Singh <anish@getmorph.com>
2025-10-25 13:38:01 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>My Simple Website</h1>
+      </header>
+      <main>
+        <p>Hello World</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
